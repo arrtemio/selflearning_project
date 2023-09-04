3ebaebb786b6cb16e9474e5c7d67c70aa7d69f62
Artsiom Hardziyonak <arrtemio@gmail.com>
2023-09-04 14:26:41 +0300

    add react refresh plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,27 +1,10 @@
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 import { buildCssLoader } from '../loaders/buildCssLoader';
+import { buildBabelLoader } from '../loaders/buildBabelLoader';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
-    const babelLoader = {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-                plugins: [
-                    ['i18next-extract', {
-                        locales: [
-                            'ru',
-                            'en',
-                        ],
-                        keyAsDefaultValue: true,
-                    }],
-                ],
-            },
-        },
-    };
+    const babelLoader = buildBabelLoader(isDev);
 
     const fileLoader = {
         test: /\.(png|jpe?g|gif|woff2|woff)$/i,
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,8 +1,8 @@
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins(
@@ -27,7 +27,7 @@ export function buildPlugins(
     ];
 
     if (isDev) {
-        plugins.push(new ReactRefreshPlugin());
+        plugins.push(new ReactRefreshWebpackPlugin());
         plugins.push(new BundleAnalyzerPlugin({
             openAnalyzer: false,
         }));
